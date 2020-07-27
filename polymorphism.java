73fdefb9a562f4794cb8bac2b844b0549a8b5403
Austin <austiny@snu.ac.kr>
2020-07-27 21:00:23 +0900

    Create polymorphism.java

---
+++ polymorphism.java
@@ -0,0 +1,18 @@
+package org.opentutorials.javatutorials.polymorphism;
+class O{
+    public void a(int param){
+        System.out.println("숫자출력");
+        System.out.println(param);
+    }
+    public void a(String param){
+        System.out.println("문자출력");
+        System.out.println(param);
+    }
+}
+public class PolymorphismOverloadingDemo {
+    public static void main(String[] args) {
+        O o = new O();
+        o.a(1);;
+        o.a("one");
+    }
+}
